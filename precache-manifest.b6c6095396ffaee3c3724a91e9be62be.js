014c01fc289c2f6ad98707e4d93d446d85393f1c
Himanshu Kansal <himanshu@ironsystems.com>
2021-07-25 01:07:39 +0530

    bluestack-deployed app

---
+++ precache-manifest.b6c6095396ffaee3c3724a91e9be62be.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a81f69ec0f1dab8383327541434ef4",
+    "url": "/bluestack-frontend-challenge/index.html"
+  },
+  {
+    "revision": "dbc46c173441cfaa8ce0",
+    "url": "/bluestack-frontend-challenge/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "c3ea67a43c4eee80b502",
+    "url": "/bluestack-frontend-challenge/static/css/main.0b7ce50a.chunk.css"
+  },
+  {
+    "revision": "dbc46c173441cfaa8ce0",
+    "url": "/bluestack-frontend-challenge/static/js/2.6fbf8601.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/bluestack-frontend-challenge/static/js/2.6fbf8601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ea67a43c4eee80b502",
+    "url": "/bluestack-frontend-challenge/static/js/main.4f8c75ce.chunk.js"
+  },
+  {
+    "revision": "b134c5b8897ac27b0c33",
+    "url": "/bluestack-frontend-challenge/static/js/runtime-main.04018f18.js"
+  },
+  {
+    "revision": "977ecf7be74437c437106e00ad13f379",
+    "url": "/bluestack-frontend-challenge/static/media/G1.977ecf7b.png"
+  },
+  {
+    "revision": "7173cfe7840a5f2d3853b4220f6cf851",
+    "url": "/bluestack-frontend-challenge/static/media/G3.7173cfe7.png"
+  },
+  {
+    "revision": "90640a8778af0217304f5effb1aa8413",
+    "url": "/bluestack-frontend-challenge/static/media/G4.90640a87.png"
+  },
+  {
+    "revision": "e51f42c349763884e809677c9634b288",
+    "url": "/bluestack-frontend-challenge/static/media/G5.e51f42c3.png"
+  },
+  {
+    "revision": "c161ba4abb12e86093bc396fd2337b9d",
+    "url": "/bluestack-frontend-challenge/static/media/G6.c161ba4a.png"
+  },
+  {
+    "revision": "8a4d68dbcc8a535baeeef974e67aaed2",
+    "url": "/bluestack-frontend-challenge/static/media/G7.8a4d68db.png"
+  },
+  {
+    "revision": "3ecfc5bd32f7be1a974631a1e152d0b3",
+    "url": "/bluestack-frontend-challenge/static/media/G8.3ecfc5bd.png"
+  },
+  {
+    "revision": "651aac69c10d0c7c4bb53d7056bbb73d",
+    "url": "/bluestack-frontend-challenge/static/media/Popup1.651aac69.png"
+  },
+  {
+    "revision": "295ccaa194056016fb8c6c8af560d1f2",
+    "url": "/bluestack-frontend-challenge/static/media/logo.295ccaa1.jpg"
+  }
+]);
